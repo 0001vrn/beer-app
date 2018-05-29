6ade86c4c477651107cedc41b936127bb32927dc
0001vrn <varun.mcs.du.2015@gmail.com>
2018-05-29 19:03:21 +0530

    enum

---
+++ src/app/core/http/enums/http-status-codes.enum.ts
@@ -0,0 +1,13 @@
+﻿export enum HttpStatusCodes {
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    RequestTimeout = 408,
+    Conflict = 409,
+    PreconditionFailed = 412,
+    InternalServerError = 500,
+    BadGateway = 502,
+    ServiceUnavailable = 503,
+    GatewayTimeout = 504  
+}
